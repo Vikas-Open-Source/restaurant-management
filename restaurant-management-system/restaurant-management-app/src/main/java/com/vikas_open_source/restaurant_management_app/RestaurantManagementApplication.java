7fd0360432071ca8a07bf7665deacd56b095447d
Shubhaangi <shubhaangi04@gmail.com>
2024-07-29 13:46:43 +0530

    Created project for restaurant management application

---
+++ restaurant-management-system/restaurant-management-app/src/main/java/com/vikas_open_source/restaurant_management_app/RestaurantManagementApplication.java
@@ -0,0 +1,13 @@
+package com.vikas_open_source.restaurant_management_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantManagementApplication.class, args);
+	}
+
+}
